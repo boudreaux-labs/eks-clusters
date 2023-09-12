71ea4af8fcab45d719ae14559a477ff4f2213690
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-09-11 20:59:36 -0700

    changing _ to - in bucket name

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "bucket_for_pipeline_test"
+  bucket = "bucket-for-pipeline-test"
   acl    = "private"
 
   control_object_ownership = true
