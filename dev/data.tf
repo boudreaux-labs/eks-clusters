d78f63414ba45747e595860eb9590f80af27774a
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-09-12 16:35:21 -0700

    trying name instead of id

--- dev/data.tf
+++ dev/data.tf
@@ -1,9 +1,12 @@
 data "aws_availability_zones" "available" {}
 
+#Terraform needs to connect the the created cluster and use the cluster credentials to create the aws-auth configmap. Add this to the file that is creating the EKS cluster.
+#https://stackoverflow.com/questions/69655605/configmaps-aws-auth-not-found
 data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
+# END
