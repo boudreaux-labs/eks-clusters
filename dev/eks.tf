e7e69a055275d100c289b003957bd24250c82364
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-09-12 20:11:35 -0700

    ?

--- dev/eks.tf
+++ dev/eks.tf
@@ -3,18 +3,18 @@
 #Terraform needs to connect the the created cluster and use the cluster credentials to create the aws-auth configmap. Add this to the file that is creating the EKS cluster. 
 #https://stackoverflow.com/questions/69655605/configmaps-aws-auth-not-found
 
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
-}
-
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_id
 }
 #END
 
