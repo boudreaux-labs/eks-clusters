6169878bc5f20f665c3b59a2fcfe6b1b2d453719
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-09-12 20:40:18 -0700

    id

--- dev/eks.tf
+++ dev/eks.tf
@@ -4,11 +4,11 @@
 #https://stackoverflow.com/questions/69655605/configmaps-aws-auth-not-found
 
 data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
