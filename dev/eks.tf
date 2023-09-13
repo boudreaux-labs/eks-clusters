be8086e735bdc8183eb0b9e38f7f2c1865aef265
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-09-13 16:10:13 -0700

    fixing bracket

--- dev/eks.tf
+++ dev/eks.tf
@@ -45,8 +45,8 @@ module "eks" {
     aws-ebs-csi-driver = {
       most_recent = true
     }  
-    }
   }
+  
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
