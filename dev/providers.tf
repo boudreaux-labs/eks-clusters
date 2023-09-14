9e0c7d369892317fbb453710fbd2989c5ced9f41
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-09-13 19:44:44 -0700

    trying this 3rd party kubectl provider

--- dev/providers.tf
+++ dev/providers.tf
@@ -24,13 +24,12 @@ provider "kubernetes" {
   }
 }
 
-provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
+
+#I may or may not need this, seems like the last error suggests I do. 
+required_providers {
+  kubectl = {
+    source  = "gavinbunney/kubectl"
+    version = ">= 1.7.0"
   }
-  load_config_file = false
 }
+
