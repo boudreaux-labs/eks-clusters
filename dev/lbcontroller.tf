1676feba82b98901cdcbd2d054335166655f6d39
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-10 14:48:30 -0800

    fixing cluster name

--- dev/lbcontroller.tf
+++ dev/lbcontroller.tf
@@ -3,7 +3,7 @@ module "eks-load-balancer-controller" {
   version = "1.2.0"
   
   # insert the 3 required variables here
-  cluster_name                     = module.eks.cluster_id
+  cluster_name                     = var.cluster_name
   cluster_identity_oidc_issuer     = module.eks.oidc_provider
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
 
