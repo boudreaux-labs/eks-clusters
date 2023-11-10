8442f49a7d1622d5708f355136f1ae597355791a
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-10 14:33:53 -0800

    fixing output name

--- dev/lbcontroller.tf
+++ dev/lbcontroller.tf
@@ -3,9 +3,9 @@ module "eks-load-balancer-controller" {
   version = "1.2.0"
   
   # insert the 3 required variables here
-  cluster_name                     = module.eks.eks_cluster_id
-  cluster_identity_oidc_issuer     = module.eks.eks_cluster_identity_oidc_issuer
-  cluster_identity_oidc_issuer_arn = module.eks.eks_cluster_identity_oidc_issuer_arn
+  cluster_name                     = module.eks.cluster_id
+  cluster_identity_oidc_issuer     = module.eks.cluster_identity_oidc_issuer
+  cluster_identity_oidc_issuer_arn = module.eks.cluster_identity_oidc_issuer_arn
 
   #optional variables
   irsa_role_create                 = true
