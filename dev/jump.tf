c2d5bfe9a4248124ce591e4051e0125afaf26184
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-12 18:51:28 -0800

    Trying to build this again, this time with jump

---
+++ dev/jump.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "jump1" {
+  ami           = "ami-005f8adf84f8c5057"   
+  instance_type = "t3.small"
+  key_name      = "rdpkey"                  # Located in EC2, Network & Security, Key Pairs
+  subnet_id = module.vpc.private_subnets
+  associate_public_ip_address = true
+  iam_instance_profile = "arn:aws:iam::842851109414:role/service-role/AWSSystemsManagerDefaultEC2InstanceManagementRole"
+
+  tags = {
+    Name = "jump1"
+  }
+
+  # Additional configuration for Windows instances
+  user_data = <<-EOF
+              <powershell>
+                # PowerShell script for Windows instance setup goes here
+                # For example, you can configure Windows features, install software, etc.
+              </powershell>
+              EOF
+}
+
+# Output the public IP address of the instance for convenience
+output "jump1_public_ip" {
+  value = aws_instance.jump1.public_ip
+}
