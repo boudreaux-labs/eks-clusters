7176a1154332b83820789560902f86cff015e10d
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-13 16:32:13 -0800

    fixing vars

--- dev/jump.tf
+++ dev/jump.tf
@@ -45,8 +45,8 @@ resource "aws_security_group" "boudreaux-labs-ec2-default-sg" {
     from_port        = 3389
     to_port          = 3389
     protocol         = "tcp"
-    cidr_blocks      = [module.vpc.cidr_block]
-    ipv6_cidr_blocks = [module.vpc.ipv6_cidr_block]
+    cidr_blocks      = [module.vpc.cidr]
+    #ipv6_cidr_blocks = [module.vpc.ipv6_cidr_block]
   }
 
   egress {
