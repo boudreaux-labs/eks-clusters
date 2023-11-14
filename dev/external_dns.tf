5d1ebb4474faf030edc6d15d3def4a0c2b42aead
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-13 19:32:25 -0800

    adding external dns

---
+++ dev/external_dns.tf
@@ -0,0 +1,8 @@
+module "eks-external-dns" {
+  source  = "lablabs/eks-external-dns/aws"
+  version = "1.2.0"
+  cluster_identity_oidc_issuer     = module.eks.oidc_provider
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  enabled = true
+  irsa_role_create = true
+}
