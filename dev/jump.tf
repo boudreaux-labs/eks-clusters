945eaafdbd8f5167d380f573137b781eaf58cdf3
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-13 16:52:44 -0800

    adding the sg to the instance

--- dev/jump.tf
+++ dev/jump.tf
@@ -1,5 +1,6 @@
+#IAM Profile and Roles for the instance
 data "aws_iam_role" "existing_iam_role" {
-  name = "awssystemsmanagerdefaultec2instancemanagementrole"
+  name = "awssystemsmanagerdefaultec2instancemanagementrole" 
 }
 
 resource "aws_iam_instance_profile" "boudreaux-labs-ec2-default" {
@@ -7,8 +8,34 @@ resource "aws_iam_instance_profile" "boudreaux-labs-ec2-default" {
     role = data.aws_iam_role.existing_iam_role.name
 }
 
+#Security group for the instance
+resource "aws_security_group" "boudreaux-labs-ec2-default-sg" {
+  name        = "boudreaux-labs-ec2-default-sg"
+  description = "Allow all outbound. Allow some inbound traffic"
+  vpc_id      = module.vpc.vpc_id
 
+  ingress {
+    description      = "RDP from VPC"
+    from_port        = 3389
+    to_port          = 3389
+    protocol         = "tcp"
+    cidr_blocks      = ["${var.cidr_block}"]
+  }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "boudreaux-labs-ec2-default-sg"
+  }
+}
+
+#The Instance
 resource "aws_instance" "jump1" {
   ami           = "ami-005f8adf84f8c5057"   
   instance_type = "t3.small"
@@ -16,6 +43,7 @@ resource "aws_instance" "jump1" {
   subnet_id = module.vpc.private_subnets[0]
   associate_public_ip_address = true
   iam_instance_profile = aws_iam_instance_profile.boudreaux-labs-ec2-default.name
+  vpc_security_group_ids = [aws_security_group.boudreaux-labs-ec2-default-sg.id]
 
   tags = {
     Name = "jump1"
@@ -35,29 +63,3 @@ output "jump1_public_ip" {
   value = aws_instance.jump1.public_ip
 }
 
-resource "aws_security_group" "boudreaux-labs-ec2-default-sg" {
-  name        = "boudreaux-labs-ec2-default-sg"
-  description = "Allow all outbound. Allow some inbound traffic"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description      = "RDP from VPC"
-    from_port        = 3389
-    to_port          = 3389
-    protocol         = "tcp"
-    cidr_blocks      = ["${var.cidr_block}"]
-    #ipv6_cidr_blocks = [module.vpc.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "boudreaux-labs-ec2-default-sg"
-  }
-}
