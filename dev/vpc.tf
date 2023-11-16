067e9ed5f260763828e44c4b6f6c522a1b895e20
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-15 21:53:07 -0800

    adding a default sg to my vpc

--- dev/vpc.tf
+++ dev/vpc.tf
@@ -23,5 +23,31 @@ module "vpc" {
     Environment = "${var.stack}"
     "kubernetes.io/role/elb" = 1
   }
-
 }
+
+resource "aws_security_group" "vpc_default_sg" {
+  vpc_id      = module.vpc.vpc_id  
+
+  name        = "vpc_default_sg"
+  description = "vpc_default_sg"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc_default_sg"
+    // Add more tags as needed
+  }
+}
+
