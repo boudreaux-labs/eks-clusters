7de01454e48f42517be324e37f9512081ed043b7
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-15 20:24:12 -0800

    Cleaning some items up

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,9 +1,3 @@
 
-output "kubeconfig_command" {
-  value       = "aws eks update-kubeconfig --name ${var.cluster_name} --region ${var.region} --role-arn <your role here>"
-  description = "eks command to update kubeconfig and connect to cluster"
-}
 
-output "argocd_url" {
-  value = local.argocd.argocd_url
-}
+
