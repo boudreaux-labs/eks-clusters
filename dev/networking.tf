e0d2f96b8500c84ec64edd9269b17aa3ef89a1aa
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-17 15:02:51 -0800

    string work

--- dev/networking.tf
+++ dev/networking.tf
@@ -98,14 +98,14 @@ module "alb_whitelisting_sg" {
       to_port     = 443
       protocol    = "tcp"
       description = "Allow inbound traffic on port 443"
-      cidr_blocks = "${var.my_ip}"
+      cidr_blocks = var.my_ip
       },
     {
       from_port   = 443
       to_port     = 443
       protocol    = "tcp"
       description = "Allow inbound traffic on port 443"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = "0.0.0.0/0"
     }
   ]
 
