5b9f1e68fa0796c94d60b0ab09170f290e48cade
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2023-11-18 10:46:49 -0800

    trying to make a new user

--- dev/argo.tf
+++ dev/argo.tf
@@ -22,13 +22,39 @@ resource "helm_release" "argocd" {
 
 #ArgoCD user management. ChatGPT helped with this one (a little, poorly)
 
-resource "kubectl_secret" "argocd_initial_admin_secret" {
+# resource "kubectl_secret" "argocd_initial_admin_secret" {
+#   metadata {
+#     name      = "argocd-initial-admin-secret"
+#     namespace = "argocd"
+#   }
+
+#   data = {
+#     "admin.password" = base64encode(var.argocd_admin_pwd)
+#   }
+# }
+
+resource "kubectl_config_map" "argocd_cm" {
   metadata {
-    name      = "argocd-initial-admin-secret"
+    name      = "argocd-cm"
+    namespace = "argocd"
+    labels = {
+      "app.kubernetes.io/name"     = "argocd-cm"
+      "app.kubernetes.io/part-of" = "argocd"
+    }
+  }
+  data = {
+    "accounts.kenny"                 = "apiKey, login"
+    "accounts.kenny.enabled"         = "true"
+  }
+}
+
+resource "kubectl_secret" "argocd_kenny_secret" {
+  metadata {
+    name      = "argocd_kenny_secret"  # Update the name if needed
     namespace = "argocd"
   }
 
   data = {
-    "admin.password" = base64encode(var.argocd_admin_pwd)
+    "accounts.kenny.password" = base64encode(var.argocd_admin_pwd)
   }
 }
