a34e25b3d06dbd1455b1c9ca833c867e6679c1f1
Kenny Boudreaux <13901902-boudreaux.kenny@users.noreply.gitlab.com>
2024-02-18 01:28:49 +0000

    adding policy

--- dev/eks_addons.tf
+++ dev/eks_addons.tf
@@ -17,7 +17,5 @@ module "eks-external-dns" {
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
   enabled = true
   irsa_role_create = true
-  additional_args = [
-    "policy:sync"
-  ]
+  policy = "sync"
 }
