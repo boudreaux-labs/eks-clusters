99c056b531e1ce7a9b860025ad297cb5bfbc2a96
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2026-02-15 16:15:17 -0800

    adding version to vpc module

--- dev/networking.tf
+++ dev/networking.tf
@@ -1,6 +1,7 @@
 #VPC
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.16.0"
 
   name                 = "${var.stack}-vpc1"
   cidr                 = "${var.cidr_block}"
