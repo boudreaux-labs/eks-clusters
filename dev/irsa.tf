e02812a5e4cd77a9a51359b9fb6d001d9171997a
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2026-02-16 14:08:35 -0800

    Switch to iam-role-for-service-accounts-eks module with correct outputs

--- dev/irsa.tf
+++ dev/irsa.tf
@@ -3,10 +3,10 @@
 ### VPN CNI
 
 module "irsa-vpc-cni" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 6.0"
 
-  name                  = "vpc_cni"
+  role_name             = "vpc_cni"
   attach_vpc_cni_policy = true
   vpc_cni_enable_ipv4   = true
 
@@ -26,10 +26,10 @@ module "irsa-vpc-cni" {
 ### EBS CSI
 
 module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 6.0"
 
-  name                  = "ebs-csi"
+  role_name             = "ebs-csi"
   attach_ebs_csi_policy = true
 
   oidc_providers = {
