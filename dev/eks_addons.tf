d0abd78fd6b93adbdc919237bb0bc18b8dde48ed
Kenny Boudreaux <boudreaux.kenny@gmail.com>
2026-02-16 16:17:41 -0800

    Remove TF_LOG DEBUG, auto-run apply, reset helm timeout to 300s

--- dev/eks_addons.tf
+++ dev/eks_addons.tf
@@ -10,7 +10,7 @@ module "eks-load-balancer-controller" {
   irsa_policy_enabled              = true
   helm_wait                        = true
   helm_wait_for_jobs               = true
-  helm_timeout                     = 600
+  helm_timeout                     = 300
 }
 
 module "eks-external-dns" {
